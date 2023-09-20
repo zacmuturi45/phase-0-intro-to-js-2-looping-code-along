c6d80efabee90f8e7313793722bd9edcb4b6e607
isaacgichuhi <zacmuturi45@gmail.com>
2023-09-20 12:17:33 +0300

    comp

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const Cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(gif, gify) {
+    for(let i = 0; i < Cards.length; i++) {
+        console.log(`Thank you, ${Cards[i]}, for the wonderful ${gify} gift!`);
+        debugger;
+    }
+    return Array = [
+        `Thank you, ${Cards[0]}, for the wonderful ${gify} gift!`,
+        `Thank you, ${Cards[1]}, for the wonderful ${gify} gift!`,
+        `Thank you, ${Cards[2]}, for the wonderful ${gify} gift!`,
+    ];
+}
+
+
+writeCards(Cards, "surprise");
